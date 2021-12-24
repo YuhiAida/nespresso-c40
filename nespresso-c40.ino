cfcae2d3c877ce8a75b985a68745cf8efc9c099c
Bernardo <YuhiAida@gmail.com>
2021-12-24 11:28:38 -0300

    Minor changes in the timing for large coffee

--- nespresso-c40.ino
+++ nespresso-c40.ino
@@ -1,11 +1,11 @@
 /*YuhiAida 
-The large coffe is set to 100ml~ while the short is set to 50ml~
+The large coffee is set to 100ml~ while the short is set to 50ml~
 */
 
-int hOffShort=2000; //Cooldown time(OFF) for the heating element in a short coffe
-int hOffLarge=4000; //Cooldown time(OFF) for the heating element in a large coffe 
-int onShort=4000; //Time the heating element is ON when a short coffe is selected
-int onLarge=9000; //Time the heating element is ON when a large coffe is selected
+int hOffShort=2000; //Cooldown time(OFF) for the heating element in a short coffee
+int hOffLarge=3000; //Cooldown time(OFF) for the heating element in a large coffee 
+int onShort=4000; //Time the heating element is ON when a short coffee is selected
+int onLarge=7000; //Time the heating element is ON when a large coffee is selected
 
 void setup() {
 pinMode(19, OUTPUT); //relay1, heater
@@ -22,11 +22,11 @@ digitalWrite(2, LOW);
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int button1=digitalRead(6); //short coffe
-  int button2=digitalRead(5); //large coffe
+  int button1=digitalRead(6); //short coffee
+  int button2=digitalRead(5); //large coffee
 
   if (button1==LOW) { //short
-    digitalWrite(3, HIGH); //turn on led for short coffe
+    digitalWrite(3, HIGH); //turn on led for short coffee
     digitalWrite(19, LOW); //Turns on the heater x secs
     digitalWrite(17, LOW); //Turns on the pump x secs
     delay(onShort);
@@ -41,12 +41,12 @@ void loop() {
     digitalWrite(19, HIGH); //Turns off heater   
     delay(2000);
     digitalWrite(17, HIGH); //Turns off pump
-    digitalWrite(3, LOW); //Turn off led for short coffe
+    digitalWrite(3, LOW); //Turn off led for short coffee
     delay(1000);
     
     
   }else if (button2==LOW) { //largo
-    digitalWrite(2, HIGH); //turn on led for short coffe
+    digitalWrite(2, HIGH); //turn on led for short coffee
     digitalWrite(19, LOW); //Turns on the heater 3 secs(pre heat)
     //delay(2000);
     digitalWrite(17, LOW); //Turns on the pump 15 secs

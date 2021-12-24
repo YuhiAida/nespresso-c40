295cfe3e2f32f3bf71f3ac20ad22a0fbbfd19efb
Bernardo <YuhiAida@gmail.com>
2021-12-24 00:58:51 -0300

    Fixed amount for short/large coffe + comments
    Change the code so the large coffe pours 100ml~ and the short 50ml~. Also added more comments + change some variable names that where in Spanish

--- nespresso-c40.ino
+++ nespresso-c40.ino
@@ -1,14 +1,18 @@
-int hofflargo=8000;
-int hoffcorto=6000;
-int onShort=9000;
-int onLarge=8500;
+/*YuhiAida 
+The large coffe is set to 100ml~ while the short is set to 50ml~
+*/
+
+int hOffShort=2000; //Cooldown time(OFF) for the heating element in a short coffe
+int hOffLarge=4000; //Cooldown time(OFF) for the heating element in a large coffe 
+int onShort=4000; //Time the heating element is ON when a short coffe is selected
+int onLarge=9000; //Time the heating element is ON when a large coffe is selected
 
 void setup() {
 pinMode(19, OUTPUT); //relay1, heater
 pinMode(17, OUTPUT); //relay2, pump
-pinMode(6, INPUT_PULLUP);  //button1 corto
+pinMode(6, INPUT_PULLUP);  //button1 short
 pinMode(5, INPUT_PULLUP);  //button2 largo
-pinMode(3, OUTPUT); //led1 corto
+pinMode(3, OUTPUT); //led1 short
 pinMode(2, OUTPUT); //led2 largo
 digitalWrite(19, HIGH);
 digitalWrite(17, HIGH);
@@ -18,20 +22,20 @@ digitalWrite(2, LOW);
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int button1=digitalRead(6); //corto
-  int button2=digitalRead(5); //largo
+  int button1=digitalRead(6); //short coffe
+  int button2=digitalRead(5); //large coffe
 
-  if (button1==LOW) { //CORTO
+  if (button1==LOW) { //short
     digitalWrite(3, HIGH); //turn on led for short coffe
     digitalWrite(19, LOW); //Turns on the heater x secs
     digitalWrite(17, LOW); //Turns on the pump x secs
     delay(onShort);
     digitalWrite(19, HIGH); //Turns off heater
-    delay(hofflargo);  //cooldown heater
+    delay(hOffShort);  //cooldown heater
     digitalWrite(19, LOW); //Turns on heater again
     delay(onShort);
     digitalWrite(19, HIGH); //Turns off heater
-    delay(hofflargo);  //cooldown heater
+    delay(hOffShort);  //cooldown heater
     digitalWrite(19, LOW); //Turns on heater again
     delay(onShort);
     digitalWrite(19, HIGH); //Turns off heater   
@@ -41,41 +45,43 @@ void loop() {
     delay(1000);
     
     
-  }else if (button2==LOW) { //LARGO
+  }else if (button2==LOW) { //largo
     digitalWrite(2, HIGH); //turn on led for short coffe
     digitalWrite(19, LOW); //Turns on the heater 3 secs(pre heat)
     //delay(2000);
     digitalWrite(17, LOW); //Turns on the pump 15 secs
     delay(onLarge);
     digitalWrite(19, HIGH); //Turns off heater
-    delay(hofflargo);  //cooldown heater
+    delay(hOffLarge);  //cooldown heater
     digitalWrite(19, LOW); //Turns on heater again
     delay(onLarge);
     digitalWrite(19, HIGH); //Turns off heater
-    delay(hofflargo);  //cooldown heater
+    delay(hOffLarge);  //cooldown heater
     digitalWrite(19, LOW); //Turns on heater again
     delay(onLarge);
     digitalWrite(19, HIGH); //Turns off heater
-    delay(hofflargo);  //cooldown heater
+    delay(hOffLarge);  //cooldown heater
+    /*
     digitalWrite(19, LOW); //Turns on heater again
     delay(onLarge);
     digitalWrite(19, HIGH); //Turns off heater
-    delay(hofflargo);  //cooldown 
+    delay(hOffLarge);  //cooldown 
     digitalWrite(19, LOW); //Turns on heater again
     delay(onLarge);
     digitalWrite(19, HIGH); //Turns off heater  
-    delay(hofflargo);  //cooldown 
+    delay(hOffLarge);  //cooldown 
     digitalWrite(19, LOW); //Turns on heater again
     delay(onLarge);
     digitalWrite(19, HIGH); //Turns off heater
-    delay(hofflargo);  //cooldown heater
+    delay(hOffLarge);  //cooldown heater
     digitalWrite(19, LOW); //Turns on heater again
     delay(onLarge);
     digitalWrite(19, HIGH); //Turns off heater
-    delay(hofflargo);  //cooldown heater
+    delay(hOffLarge);  //cooldown heater
     digitalWrite(19, LOW); //Turns on heater again
     delay(onLarge);
     digitalWrite(19, HIGH); //Turns off heater    
+    */
     delay(2000);
     digitalWrite(17, HIGH); //Turns off pump
     digitalWrite(2, LOW);

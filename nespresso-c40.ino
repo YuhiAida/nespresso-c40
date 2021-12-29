e6bd39f61961794baeb33dbb52fb6081b6c6726e
Bernardo <YuhiAida@gmail.com>
2021-12-29 15:08:10 -0300

    Revert "Merge pull request #1 from YuhiAida/new-feature"
    This reverts commit 43da48b8e146098e50642bcd86f2f68e23fb90b9, reversing
    changes made to 098427e4378e54631163df18273cd7dd33971e5c.

--- nespresso-c40.ino
+++ nespresso-c40.ino
@@ -1,4 +1,3 @@
-//Version 2.0
 /*YuhiAida 
 The large coffee is set to 100ml~ while the short is set to 50ml~
 */
@@ -8,66 +7,6 @@ int hOffLarge=3000; //Cooldown time(OFF) for the heating element in a large coff
 int onShort=4000; //Time the heating element is ON when a short coffee is selected
 int onLarge=7000; //Time the heating element is ON when a large coffee is selected
 
-
-//Test new feature
-
-const byte keyPin = 5; // The switch is connected to pin number 5
-int pressTime = 0; // the counter that acounnt the pressing time
-
-void setup() {
-
-pinMode(keyPin, INPUT_PULLUP);
-pinMode(19, OUTPUT); //relay1, heater
-pinMode(17, OUTPUT); //relay2, pump
-pinMode(6, INPUT_PULLUP);  //button1 short
-pinMode(3, OUTPUT); //led1 short
-pinMode(2, OUTPUT); //led2 largo
-digitalWrite(19, HIGH);
-digitalWrite(17, HIGH);
-digitalWrite(3, LOW);
-digitalWrite(2, LOW);
-}
-
-void loop() {
-byte key = digitalRead(keyPin);
-if (key == LOW) { // that means the key is pressed
-pressTime++;
-delay(1000); // works like a timer with one second steps
-if (pressTime <= 2){
-  for (int i = 0; i < 10; i++)
-  {
-    digitalWrite(2,HIGH);
-    digitalWrite(17, LOW); //Turns on the pump x secs
-    delay(300);
-    digitalWrite(17, HIGH); //Turns off the pump
-    digitalWrite(2,LOW);
-    delay(300);
-  }
-  pressTime=0;
-}else{
-    digitalWrite(2, HIGH); //turn on led for large coffee
-    digitalWrite(17, LOW); //turn on the pump
-  for (int i = 0; i < 9; i++)
-  {
-    digitalWrite(19, LOW); //turn on the heater 
-    delay(onLarge);
-    digitalWrite(19, HIGH); //turn off the heater
-    delay(hOffLarge);  //cooldown heater
-  }
-  digitalWrite(2, LOW);
-  digitalWrite(17, HIGH);
-  digitalWrite(19, HIGH);
-}
-}
-else if (key == HIGH) {// key is not pressed
-pressTime=0;
-}
-
-}
-
-//End of test
-
-/*
 void setup() {
 pinMode(19, OUTPUT); //relay1, heater
 pinMode(17, OUTPUT); //relay2, pump
@@ -82,10 +21,7 @@ digitalWrite(2, LOW);
 }
 
 void loop() {
-
- 
-  
-
+  // put your main code here, to run repeatedly:
   int button1=digitalRead(6); //short coffee
   int button2=digitalRead(5); //large coffee
 
@@ -110,7 +46,7 @@ void loop() {
     
     
   }else if (button2==LOW) { //largo
-    digitalWrite(2, HIGH); //turn on led for large coffee
+    digitalWrite(2, HIGH); //turn on led for short coffee
     digitalWrite(19, LOW); //Turns on the heater 3 secs(pre heat)
     //delay(2000);
     digitalWrite(17, LOW); //Turns on the pump 15 secs
@@ -146,10 +82,10 @@ void loop() {
     delay(onLarge);
     digitalWrite(19, HIGH); //Turns off heater    
     */
-    //delay(2000);
-    //digitalWrite(17, HIGH); //Turns off pump
-    //digitalWrite(2, LOW);
-    //delay(1000);
-    //}
+    delay(2000);
+    digitalWrite(17, HIGH); //Turns off pump
+    digitalWrite(2, LOW);
+    delay(1000);
+    }
 
-//}
+}

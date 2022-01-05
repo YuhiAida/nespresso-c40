8c31f023896d43040004aa1a42dd70ffad7fd7ec
Bernardo <YuhiAida@gmail.com>
2022-01-05 20:45:22 -0300

    Added 2 functions per button
    The timing for the pump and heating element is still in testing.
    Added library for the functionality of the buttons. Now when press short coffee it will run the pump so it fill when  its dry, and the large coffee will act as a pre-heat for the water.
    The code was further organized and optimized

--- nespresso-c40.ino
+++ nespresso-c40.ino
@@ -2,63 +2,137 @@
 The large coffee is set to 100ml~ while the short is set to 50ml~
 */
 
+#include "ClickButton.h"
+
+//ShortCoffee
+const int buttonPin1 = 6; 
+ClickButton button1(buttonPin1, LOW, CLICKBTN_PULLUP);
+
+//LargeCoffee
+const int buttonPin2 = 5; 
+ClickButton button2(buttonPin2, LOW, CLICKBTN_PULLUP);
+
+// Arbitrary LED function 
+int function1 = 0;
+int function2 = 0;
+
 int hOffShort=2000; //Cooldown time(OFF) for the heating element in a short coffee
 int hOffLarge=3000; //Cooldown time(OFF) for the heating element in a large coffee 
 int onShort=4000; //Time the heating element is ON when a short coffee is selected
 int onLarge=7000; //Time the heating element is ON when a large coffee is selected
 
-void setup() {
-pinMode(19, OUTPUT); //relay1, heater
-pinMode(17, OUTPUT); //relay2, pump
-pinMode(6, INPUT_PULLUP);  //button1 short
-pinMode(5, INPUT_PULLUP);  //button2 largo
-pinMode(3, OUTPUT); //led1 short
-pinMode(2, OUTPUT); //led2 largo
-digitalWrite(19, HIGH);
-digitalWrite(17, HIGH);
-digitalWrite(3, LOW);
-digitalWrite(2, LOW);
+void setup()
+{
+  pinMode(3, OUTPUT);
+  pinMode(2, OUTPUT); 
+  pinMode(19, OUTPUT); //relay1, heater
+  pinMode(17, OUTPUT); //relay2, pump 
+  digitalWrite(19, HIGH);
+  digitalWrite(17, HIGH);
+
+  // Setup button timers (all in milliseconds / ms)
+  // (These are default if not set, but changeable for convenience)
+  button1.debounceTime   = 20;   // Debounce timer in ms
+  button1.multiclickTime = 250;  // Time limit for multi clicks
+  button1.longClickTime  = 1000; // time until "held-down clicks" register
+  button2.debounceTime   = 20;   // Debounce timer in ms
+  button2.multiclickTime = 250;  // Time limit for multi clicks
+  button2.longClickTime  = 1000; // time until "held-down clicks" register
 }
 
-void loop() {
-  int button1=digitalRead(6); //short coffee
-  int button2=digitalRead(5); //large coffee
+//  This is used in case you run out of water in the tank, the pump just won't load until you use this function
+void LoadPump()
+{
+  for (int i = 0; i < 10; i++)
+  {
+    digitalWrite(17, LOW);
+    delay(50);
+    digitalWrite(17, HIGH);
+    delay(50);
+  }
+  
+}
 
-  if (button1==LOW) { //short coffee
+//  Now that the machine doesn't have a temperature regulator the first cup will be colder so 
+//  with this you can heat the water and the heater before your first coffe or if you didn't use the machine for a period of time
+void HeatWatter()
+{
+  digitalWrite(19, LOW);
+  delay(3000);
+  digitalWrite(19, HIGH);
+}
 
-    digitalWrite(3, HIGH); //turn on led for short coffee
-    digitalWrite(17, LOW); //Turns on the pump 
-    digitalWrite(19, LOW); //Turns on the heater
+void ShortCoffe()
+{
+  digitalWrite(3, HIGH); //turn on led for short coffee
+  digitalWrite(17, LOW); //Turns on the pump 
+  digitalWrite(19, LOW); //Turns on the heater
 
-      for (int i = 0; i < 5; i++)
-      {
-        delay(hOffShort); //Cooldown heater
-        digitalWrite(19, LOW); //Turns on the heater
-        delay(onShort); //Keep the heater on
-        digitalWrite(19, HIGH); //Turns off heater
-      }
-      //End loop
-      delay(500);
-    digitalWrite(3, LOW); //Turn off led for short coffee
-    digitalWrite(17, HIGH); //Turns off pump
-    
-  }else if (button2==LOW) { //large coffee
-
-    digitalWrite(2, HIGH); //turn on led for large coffee
-    digitalWrite(17, LOW); //Turns on the pump 
-    digitalWrite(19, LOW); //Turns on the heater
-
-      for (int i = 0; i < 8; i++)
-      {
-        delay(hOffLarge);  //cooldown heater
-        digitalWrite(19, LOW); //Turns on the heater
-        delay(onLarge); //Keep the heater on
-        digitalWrite(19, HIGH); //Turns off heater
-      }
-      //End loop
-    delay(500);
-    digitalWrite(17, HIGH); //Turns off pump
-    digitalWrite(2, LOW); //Turn off led for large coffee
+    for (int i = 0; i < 2; i++)
+    {
+      delay(hOffShort); //Cooldown heater
+      digitalWrite(19, LOW); //Turns on the heater
+      delay(onShort); //Keep the heater on
+      digitalWrite(19, HIGH); //Turns off heater
     }
+    delay(500);
+  digitalWrite(3, LOW); //Turn off led for short coffee
+  digitalWrite(17, HIGH); //Turns off pump
+}
 
+void LargeCoffee()
+{
+  digitalWrite(2, HIGH); //turn on led for large coffee
+  digitalWrite(17, LOW); //Turns on the pump 
+  digitalWrite(19, LOW); //Turns on the heater
+
+    for (int i = 0; i < 8; i++)
+    {
+      delay(hOffLarge);  //cooldown heater
+      digitalWrite(19, LOW); //Turns on the heater
+      delay(onLarge); //Keep the heater on
+      digitalWrite(19, HIGH); //Turns off heater
+    }
+  delay(500);
+  digitalWrite(17, HIGH); //Turns off pump
+  digitalWrite(2, LOW); //Turn off led for large coffee  
+}
+
+void loop()
+{
+  // Update button state
+  button1.Update();
+  button2.Update();
+
+  // Save click codes in function1/2, as click codes are reset at next Update()
+  if (button1.clicks != 0) function1 = button1.clicks;
+  if (button2.clicks != 0) function2 = button2.clicks;
+
+  switch (function1)
+  {
+  case 1:
+    ShortCoffe();
+    function1 = 0;
+    break;
+  case -1:
+    LoadPump();
+    function1 = 0;
+    break;
+  default:
+    break;
+  }
+
+  switch (function2)
+  {
+  case 1:
+    LargeCoffee();
+    function2 = 0;
+    break;
+  case -1:
+    HeatWatter();
+    function2 = 0;
+    break;
+  default:
+    break;
+  }
 }

dbef60297a72be795086f18c7264291543e60dd4
Bernardo <YuhiAida@gmail.com>
2022-01-05 23:15:36 -0300

    Update the time for each coffe and added other function to the buttons

--- nespresso-c40.ino
+++ nespresso-c40.ino
@@ -19,7 +19,7 @@ int function2 = 0;
 int hOffShort=2000; //Cooldown time(OFF) for the heating element in a short coffee
 int hOffLarge=3000; //Cooldown time(OFF) for the heating element in a large coffee 
 int onShort=4000; //Time the heating element is ON when a short coffee is selected
-int onLarge=7000; //Time the heating element is ON when a large coffee is selected
+int onLarge=5000; //Time the heating element is ON when a large coffee is selected
 
 void setup()
 {
@@ -43,14 +43,18 @@ void setup()
 //  This is used in case you run out of water in the tank, the pump just won't load until you use this function
 void LoadPump()
 {
-  for (int i = 0; i < 10; i++)
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+
+  for (int i = 0; i < 20; i++)
   {
     digitalWrite(17, LOW);
     delay(50);
     digitalWrite(17, HIGH);
     delay(50);
   }
-  
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
 }
 
 //  Now that the machine doesn't have a temperature regulator the first cup will be colder so 
@@ -62,13 +66,24 @@ void HeatWatter()
   digitalWrite(19, HIGH);
 }
 
+void CooldownHeater()
+{
+  digitalWrite(3, HIGH);
+  digitalWrite(2, HIGH);
+  digitalWrite(17, LOW);
+  delay(10000);
+  digitalWrite(17, HIGH);
+  digitalWrite(3, LOW);
+  digitalWrite(2, LOW);
+}
+
 void ShortCoffe()
 {
   digitalWrite(3, HIGH); //turn on led for short coffee
   digitalWrite(17, LOW); //Turns on the pump 
   digitalWrite(19, LOW); //Turns on the heater
 
-    for (int i = 0; i < 2; i++)
+    for (int i = 0; i < 5; i++)
     {
       delay(hOffShort); //Cooldown heater
       digitalWrite(19, LOW); //Turns on the heater
@@ -86,7 +101,7 @@ void LargeCoffee()
   digitalWrite(17, LOW); //Turns on the pump 
   digitalWrite(19, LOW); //Turns on the heater
 
-    for (int i = 0; i < 8; i++)
+    for (int i = 0; i < 10; i++)
     {
       delay(hOffLarge);  //cooldown heater
       digitalWrite(19, LOW); //Turns on the heater
@@ -130,6 +145,10 @@ void loop()
     break;
   case -1:
     HeatWatter();
+    function2 = 0;
+    break;
+  case 2:
+    CooldownHeater();
     function2 = 0;
     break;
   default:
